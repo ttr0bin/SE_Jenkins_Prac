56fdc93e91ec4b0ef5caa89a3ddda0f85b336605
ttr0bin <monica990131@gmail.com>
2025-05-21 11:13:40 +0900

    Initial commit

---
+++ JUnitPrac/src/main/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package main.calculator;
+
+public class Calculator {
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public String add(String str1, String str2) {
+        return str1 + str2;
+    }
+}
---
+++ JUnitPrac/test/calculator/CalculatorTest.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import main.calculator.Calculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void testAddIntInt() {
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(0, calculator.add(-2, 2));
+    }
+
+    @Test
+    void testAddStringString() {
+        assertEquals("hello world", calculator.add("hello ", "world"));
+        assertEquals("abc", calculator.add("a", "bc"));
+    }
+
+    // 아직 테스트 작성하지 않은 경우 함수 내용
+    // fail("Not yet implemented");
+}
